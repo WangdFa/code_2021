8386052a0c374fc562b5d0f2d1d79f1b9e053c02
qinyulong <qinyulong@copowers.cn>
2021-05-24 16:06:34 +0800

    汇能公司分组程序

--- group.py
+++ group.py
@@ -5,17 +5,17 @@ data = list(data['参加人员'].values)
 a = 0
 num = 0
 temp_group = []
-while num < 4:
+while num < 5:
     temp_person = random.choice(data)
     data.remove(temp_person)
     temp_group.append(temp_person)
     a += 1
-    if a==10 and num!=3 :
+    if a==8 and num!=4 :
         print("第{}组成员为:".format(num+1),temp_group)
         num += 1
         a = 0
         temp_group = []
-    elif a>10 :
+    elif a>8 :
         print("第{}组成员为:".format(num + 1), temp_group)
         num += 1
 
